414f9bccbc08ec2e6c9cc6a1bdba147aa63490ed
test <dusanarizanovic@yahoo.com>
2019-02-22 17:32:21 +0100

    odradjeni neki kontroleri ispisa, zapocet kontroler upisa preko get-a

---
+++ demo_dusan3/src/main/java/com/example/demo/api/TerminController.java
@@ -0,0 +1,23 @@
+package com.example.demo.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.TerminDao;
+import com.example.demo.domain.Termin;
+
+	@RestController
+	@RequestMapping("/trener")
+	public class TerminController {
+			
+			@Autowired
+			TerminDao terminDao;
+			
+			@RequestMapping(value="/termin/save", method=RequestMethod.GET)
+			public @RequestBody String process (@RequestParam String ime, @RequestParam String prezime, )
+			
+		}
