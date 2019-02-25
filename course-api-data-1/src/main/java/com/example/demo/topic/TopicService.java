6ac942728b42feb22c989af59621516cf3cb44cb
retardinjo <dusanarizanovic@yahoo.com>
2019-02-25 10:04:16 +0100

    get clan by id, clancontroller

--- course-api-data-1/src/main/java/com/example/demo/topic/TopicService.java
+++ course-api-data-1/src/main/java/com/example/demo/topic/TopicService.java
@@ -1,8 +1,8 @@
 package com.example.demo.topic;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,15 +12,8 @@ public class TopicService {
 
 	@Autowired
 	private TopicRepository topicRepository;
-	
-	private List<Topic> topics = new ArrayList<>(Arrays.asList(
-			new Topic("spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("java", "Core Java", "Core Java description"),
-			new Topic("javascript", "Java Script", "Javascript Description")		
-			));
-	
+
 	public List<Topic> getAllTopics() {
-		// return topics;
 		List<Topic> topics = new ArrayList<>();
 		topicRepository.findAll()
 		.forEach(topics::add);
@@ -28,8 +21,9 @@ public class TopicService {
 	}
 	
 	
-	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	public Optional<Topic> getTopic(String id) {
+		// return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id);
 
 	}
 
@@ -41,19 +35,13 @@ public class TopicService {
 
 
 	public void updateTopic(String id, Topic topic) {
-		for(int i=0; i<topics.size(); i++) {
-			Topic t = topics.get(i);
-			if(t.getId().equals(id)) {
-				topics.set(i, topic);
-				return;
-			}
-		}
+		topicRepository.save(topic);
 		
 	}
 
 
 	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.deleteById(id);
 
 	}
 }
--- demo_dusan3/src/main/java/com/example/demo/controller/ClanController.java
+++ demo_dusan3/src/main/java/com/example/demo/controller/ClanController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,11 +31,44 @@ public class ClanController {
 		c.setGodine(godine);
 		c.setEmail(email);		
 		clanDao.save(c);
-		return "Clanfindall saved";	
+		return "Clan saved";	
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/update/{id}")
+	public @ResponseBody String process(@PathVariable Long id, @RequestParam String ime, @RequestParam String prezime, @RequestParam String pol, @RequestParam Integer godine, @RequestParam String email)  {		
+
+		Clan c = new Clan();
+		c.setIme(ime);
+		c.setPrezime(prezime);
+		c.setPol(pol);
+		c.setGodine(godine);
+		c.setEmail(email);		
+		clanDao.save(c);
+		return "Clan updated";	
 	}
 	
 	@GetMapping("/findall")
 	public List<Clan> test() {		
 		return (List<Clan>)clanDao.findAll();
 	}
+	
+	@RequestMapping("/find/{id}")
+	public Optional<Clan> getClan(@PathVariable Long id) {
+		return (Optional<Clan>)clanDao.findById(id);
+		
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
