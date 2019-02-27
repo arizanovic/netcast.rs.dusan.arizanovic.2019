d489806e7965c99df886ba82b859d75ca5a6340a
retardinjo <dusanarizanovic@yahoo.com>
2019-02-27 21:00:06 +0100

    baza kuci

--- teretana_d3.sql
+++ teretana_d3.sql
@@ -30,7 +30,7 @@ CREATE TABLE `clan` (
   `godine` int(11) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,7 @@ CREATE TABLE `clan` (
 
 LOCK TABLES `clan` WRITE;
 /*!40000 ALTER TABLE `clan` DISABLE KEYS */;
+INSERT INTO `clan` VALUES (1,'zzdusandto','kuciarizanovicdto','kucimuskidto',34,'kucid@yahoo.comdto'),(2,'sdfg','sdfg','zenski',3245,'asdf'),(4,'kucidusandto','kuciarizanovicdto','kucimuskidto',34,'kucid@yahoo.comdto'),(5,'5','5','5',5,'5@yahoo.com'),(6,'1ooodusanupdated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated'),(7,'2ooodusanupdated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated'),(8,'3ooodusanupdated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated'),(9,'updated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated');
 /*!40000 ALTER TABLE `clan` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,16 +52,16 @@ DROP TABLE IF EXISTS `pretplata`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pretplata` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `pocetak` datetime DEFAULT NULL,
-  `kraj` datetime DEFAULT NULL,
-  `clan_id` int(11) DEFAULT NULL,
-  `tip_pretplate_id` int(11) DEFAULT NULL,
+  `pocetak` datetime NOT NULL,
+  `kraj` datetime NOT NULL,
+  `clan_id` int(11) NOT NULL,
+  `tip_pretplate_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `tip_pretplate_idx` (`tip_pretplate_id`),
   KEY `clan_idx` (`clan_id`),
-  CONSTRAINT `clan` FOREIGN KEY (`clan_id`) REFERENCES `clan` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `tip_pretplate` FOREIGN KEY (`tip_pretplate_id`) REFERENCES `tip_pretplate` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `clan` FOREIGN KEY (`clan_id`) REFERENCES `clan` (`id`),
+  CONSTRAINT `tip_pretplate` FOREIGN KEY (`tip_pretplate_id`) REFERENCES `tip_pretplate` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,6 +70,7 @@ CREATE TABLE `pretplata` (
 
 LOCK TABLES `pretplata` WRITE;
 /*!40000 ALTER TABLE `pretplata` DISABLE KEYS */;
+INSERT INTO `pretplata` VALUES (1,'2019-02-11 00:00:00','2019-02-11 00:00:00',2,2),(2,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,1),(4,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,1),(5,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,1),(6,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,1),(7,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,1),(8,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,2);
 /*!40000 ALTER TABLE `pretplata` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,7 +92,7 @@ CREATE TABLE `termin` (
   KEY `trener_idx` (`trener_id`),
   CONSTRAINT `clan1` FOREIGN KEY (`clan_id`) REFERENCES `clan` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `trener` FOREIGN KEY (`trener_id`) REFERENCES `trener` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,6 +101,7 @@ CREATE TABLE `termin` (
 
 LOCK TABLES `termin` WRITE;
 /*!40000 ALTER TABLE `termin` DISABLE KEYS */;
+INSERT INTO `termin` VALUES (1,'2019-01-15 00:00:00','2019-01-15 00:00:00',1,1),(2,'2019-02-15 00:00:00','2019-02-15 00:00:00',2,2);
 /*!40000 ALTER TABLE `termin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -114,7 +117,7 @@ CREATE TABLE `tip_pretplate` (
   `tip` varchar(45) DEFAULT NULL,
   `cena` double DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,6 +126,7 @@ CREATE TABLE `tip_pretplate` (
 
 LOCK TABLES `tip_pretplate` WRITE;
 /*!40000 ALTER TABLE `tip_pretplate` DISABLE KEYS */;
+INSERT INTO `tip_pretplate` VALUES (1,'fghhhhhhhhhhhhhhhhhh',222222222222),(2,'mesecna',321);
 /*!40000 ALTER TABLE `tip_pretplate` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,7 +143,7 @@ CREATE TABLE `trener` (
   `prezime` varchar(45) DEFAULT NULL,
   `cena` double DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,6 +152,7 @@ CREATE TABLE `trener` (
 
 LOCK TABLES `trener` WRITE;
 /*!40000 ALTER TABLE `trener` DISABLE KEYS */;
+INSERT INTO `trener` VALUES (1,'milan','krkobabic-junior',1300),(2,'23','sdfgs',123);
 /*!40000 ALTER TABLE `trener` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -160,4 +165,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-22 12:00:47
+-- Dump completed on 2019-02-27 20:57:48
