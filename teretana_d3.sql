c9c6906f5ec5ce6b5e8ae41da529768cde896487
retardinjo <dusanarizanovic@yahoo.com>
2019-03-07 12:05:57 +0100

    novi export baze sa dodatim tabelama snizenje i korisnik

--- teretana_d3.sql
+++ teretana_d3.sql
@@ -29,7 +29,13 @@ CREATE TABLE `clan` (
   `pol` varchar(45) DEFAULT NULL,
   `godine` int(11) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `snizenje_id` int(11) NOT NULL,
+  `pretplata_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `snizenje_idx` (`snizenje_id`),
+  KEY `pretplata_idx` (`pretplata_id`),
+  CONSTRAINT `pretplata` FOREIGN KEY (`pretplata_id`) REFERENCES `pretplata` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `snizenje` FOREIGN KEY (`snizenje_id`) REFERENCES `snizenje` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -39,8 +45,36 @@ CREATE TABLE `clan` (
 
 LOCK TABLES `clan` WRITE;
 /*!40000 ALTER TABLE `clan` DISABLE KEYS */;
-INSERT INTO `clan` VALUES (1,'zzdusandto','kuciarizanovicdto','kucimuskidto',34,'kucid@yahoo.comdto'),(2,'sdfg','sdfg','zenski',3245,'asdf'),(4,'kucidusandto','kuciarizanovicdto','kucimuskidto',34,'kucid@yahoo.comdto'),(5,'5','5','5',5,'5@yahoo.com'),(6,'1ooodusanupdated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated'),(7,'2ooodusanupdated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated'),(8,'3ooodusanupdated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated'),(9,'updated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated');
+INSERT INTO `clan` VALUES (1,'zzdusandto','kuciarizanovicdto','kucimuskidto',34,'kucid@yahoo.comdto',1,1),(2,'sdfg','sdfg','zenski',3245,'asdf',1,1),(4,'kucidusandto','kuciarizanovicdto','kucimuskidto',34,'kucid@yahoo.comdto',1,1),(5,'5','5','5',5,'5@yahoo.com',1,1),(6,'1ooodusanupdated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated',1,1),(7,'2ooodusanupdated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated',1,1),(8,'3ooodusanupdated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated',1,1),(9,'updated','oooarizanovicupdated','oomuskiupdated',344444,'ooodusan@yahoo.comupdated',1,1);
 /*!40000 ALTER TABLE `clan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `korisnik`
+--
+
+DROP TABLE IF EXISTS `korisnik`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `korisnik` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ime` varchar(45) DEFAULT NULL,
+  `prezime` varchar(45) DEFAULT NULL,
+  `korisnicko_ime` varchar(45) DEFAULT NULL,
+  `sifra` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `korisnik`
+--
+
+LOCK TABLES `korisnik` WRITE;
+/*!40000 ALTER TABLE `korisnik` DISABLE KEYS */;
+INSERT INTO `korisnik` VALUES (1,'jovan','krkobabic','mladic','mladic123','lkj@yahoo.com'),(2,'11zivojin','krkobabic','mladic','mladic123','lkj@yahoo.com'),(3,'zivojin','krkobabic','mladic','mladic123','lkj@yahoo.com');
+/*!40000 ALTER TABLE `korisnik` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -54,12 +88,9 @@ CREATE TABLE `pretplata` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pocetak` datetime NOT NULL,
   `kraj` datetime NOT NULL,
-  `clan_id` int(11) NOT NULL,
   `tip_pretplate_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `tip_pretplate_idx` (`tip_pretplate_id`),
-  KEY `clan_idx` (`clan_id`),
-  CONSTRAINT `clan` FOREIGN KEY (`clan_id`) REFERENCES `clan` (`id`),
   CONSTRAINT `tip_pretplate` FOREIGN KEY (`tip_pretplate_id`) REFERENCES `tip_pretplate` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -70,8 +101,33 @@ CREATE TABLE `pretplata` (
 
 LOCK TABLES `pretplata` WRITE;
 /*!40000 ALTER TABLE `pretplata` DISABLE KEYS */;
-INSERT INTO `pretplata` VALUES (1,'2019-02-11 00:00:00','2019-02-11 00:00:00',2,2),(2,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,1),(4,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,1),(5,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,1),(6,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,1),(7,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,1),(8,'2019-02-11 00:00:00','2019-02-11 00:00:00',1,2);
+INSERT INTO `pretplata` VALUES (1,'2019-02-11 00:00:00','2019-02-11 00:00:00',2),(2,'2019-02-11 00:00:00','2019-02-11 00:00:00',1),(4,'2019-02-11 00:00:00','2019-02-11 00:00:00',1),(5,'2019-02-11 00:00:00','2019-02-11 00:00:00',1),(6,'2019-02-11 00:00:00','2019-02-11 00:00:00',1),(7,'2019-02-11 00:00:00','2019-02-11 00:00:00',1),(8,'2019-02-11 00:00:00','2019-02-11 00:00:00',2);
 /*!40000 ALTER TABLE `pretplata` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `snizenje`
+--
+
+DROP TABLE IF EXISTS `snizenje`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `snizenje` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `snizenje` double DEFAULT NULL,
+  `tip` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `snizenje`
+--
+
+LOCK TABLES `snizenje` WRITE;
+/*!40000 ALTER TABLE `snizenje` DISABLE KEYS */;
+INSERT INTO `snizenje` VALUES (1,0.4,'plavuse');
+/*!40000 ALTER TABLE `snizenje` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -85,14 +141,17 @@ CREATE TABLE `termin` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pocetak` datetime DEFAULT NULL,
   `kraj` datetime DEFAULT NULL,
-  `clan_id` int(11) DEFAULT NULL,
-  `trener_id` int(11) DEFAULT NULL,
+  `clan_id` int(11) NOT NULL,
+  `trener_id` int(11) NOT NULL,
   `datum` date DEFAULT NULL,
   `cena` double DEFAULT NULL,
+  `korisnik_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `clan1_idx` (`clan_id`),
   KEY `trener_idx` (`trener_id`),
+  KEY `operater` (`korisnik_id`),
   CONSTRAINT `clan1` FOREIGN KEY (`clan_id`) REFERENCES `clan` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `korisnik` FOREIGN KEY (`korisnik_id`) REFERENCES `korisnik` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `trener` FOREIGN KEY (`trener_id`) REFERENCES `trener` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -103,7 +162,7 @@ CREATE TABLE `termin` (
 
 LOCK TABLES `termin` WRITE;
 /*!40000 ALTER TABLE `termin` DISABLE KEYS */;
-INSERT INTO `termin` VALUES (1,'2019-01-15 00:00:00','2019-01-15 00:00:00',1,1,'2019-01-15',2345),(2,'2019-02-15 00:00:00','2019-02-15 00:00:00',2,2,'2019-02-15',6758);
+INSERT INTO `termin` VALUES (1,'2019-01-15 00:00:00','2019-01-15 00:00:00',1,1,'2019-01-15',2345,1),(2,'2019-02-15 00:00:00','2019-02-15 00:00:00',2,2,'2019-02-15',6758,1);
 /*!40000 ALTER TABLE `termin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -167,4 +226,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-01 11:34:58
+-- Dump completed on 2019-03-07 12:05:04
